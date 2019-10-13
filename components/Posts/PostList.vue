380f386a059c5468f90c71e1e1354e19a82ef824
damar huda <damarhuda@live.com>
2019-10-13 13:17:14 +0700

    first commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://4533x635aus61yamnx2wkfwe-wpengine.netdna-ssl.com/wp-content/uploads/2019/08/shutterstock_1453123070-898x505.jpg"
+      title="Hello there"
+      previewText="THis mu fisrt post 1"
+    />
+    <PostPreview
+      id="2"
+      :is-admin="isAdmin"
+      thumbnail="https://4533x635aus61yamnx2wkfwe-wpengine.netdna-ssl.com/wp-content/uploads/2019/08/shutterstock_1453123070-898x505.jpg"
+      title="Hello there 2"
+      previewText="THis mu fisrt post 2"
+    />
+    <PostPreview
+      id="3"
+      :is-admin="isAdmin"
+      thumbnail="https://4533x635aus61yamnx2wkfwe-wpengine.netdna-ssl.com/wp-content/uploads/2019/08/shutterstock_1453123070-898x505.jpg"
+      title="Hello there 3"
+      previewText="THis mu fisrt post 3"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+export default {
+    components: {
+        PostPreview
+    },
+    props: {
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost"/>
+        </section>
+    </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+export default {
+    layout: 'admin',
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Damar',
+                title: 'My Awesome Post',
+                content: 'super amazing',
+                thumbnailLink: 'https://4533x635aus61yamnx2wkfwe-wpengine.netdna-ssl.com/wp-content/uploads/2019/08/shutterstock_1453123070-898x505.jpg'
+
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
